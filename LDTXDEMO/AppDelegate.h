049022ec4200774759b8ee94f12033ef5d457518
dongdong <159827456045@163.com>
2018-01-16 09:41:14 +0800

    Initial Commit

---
+++ LDTXDEMO/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LDTXDEMO
+//
+//  Created by ios on 2018/1/16.
+//  Copyright © 2018年 LuoDong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LDTXDEMO/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LDTXDEMO
+//
+//  Created by ios on 2018/1/16.
+//  Copyright © 2018年 LuoDong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
